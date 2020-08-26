354532b1f3dd825970e547d392b4da83b83c1bcd
juniorcapra <acaprajr@gmail.com>
2020-08-26 20:56:07 -0300

    Create JPA repository

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,5 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +17,22 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private ExchangeValueRepository repository;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+    public ExchangeValue retrieveExchangeValue
+        (@PathVariable String from, @PathVariable String to){
 
-        ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
-        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        ExchangeValue exchangeValue =
+            repository.findByFromAndTo(from, to);
+
+        exchangeValue.setPort(
+            Integer.parseInt(environment.getProperty("local.server.port")));
+
+        logger.info("{}", exchangeValue);
+
         return exchangeValue;
-
     }
-
 
 }
